2eb9c6f5127a46ef08d44d1314870ad92bbb9732
Vanessa3123 <karla.lopez.navarro@cuc.cr>
2021-10-14 19:53:12 -0600

    Se agrega la variable juego

--- Juego_ElTonto/FrmMenuPrincipal.vb
+++ Juego_ElTonto/FrmMenuPrincipal.vb
@@ -31,4 +31,14 @@
     Private Sub InstrucccionesToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles InstrucccionesToolStripMenuItem.Click
         frmInstrucciones.Show()
     End Sub
+
+    Private Sub JuegoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles JuegoToolStripMenuItem.Click
+        Dim juego As String
+
+
+    End Sub
+
+    Private Sub FrmMenuPrincipal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
